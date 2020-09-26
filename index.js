97d93b8e39acfa5f2a9ecf4d6cc5b8785ba9c9d0
unknown <josue.gomez2021@poma.superate.org.sv>
2020-09-26 18:06:37 -0500

    codigo para obtener el contacto de las ONGs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const VolunteersControllers = require("./Controllers/VolunteersControllers");
+const volunteeringpsControllers = require("./Controllers/volunteeringpsControllers");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-app.use("/Volunteers", VolunteersControllers);
+app.use("/volunteeringps:code", volunteeringpsControllers);
 
 app.listen(3000, function () {
-  console.log("Cool Zone in port 3000!");
+  console.log("Server on port 3000!");
 });
---
+++ volunteeringpsControllers.js
@@ -0,0 +1,56 @@
+const { Router, response } = require ("express");
+
+class VolunteeringpsController {
+    router = Router ();
+
+    constructor() {
+        this.getvolunteeringps();
+        this.createvolunteeringps();
+}
+};
+
+getvolunteeringps = () => {
+    this.router.get("/volunteeringps:code", (request, response) => {
+      var code = request.params.code;
+      var volunteeringps = {},
+      var statusCode = 200,
+
+      if (code == "07GESA") {
+        volunteeringps = {
+          "name": "Glasswing internacional",
+          "contacto": "nloucel@glasswing.org",
+        };
+      } else if (code == "01TESA") {
+        volunteeringps = {
+        "name": "TECHO El Salvador",
+        "contacto": "7429 7484",
+        };
+      } else if (code == "07FDSESA") {
+        volunteeringps = {
+          "name": "Fabrica de sonrisas",
+          "contacto": "fds.es.info@gmail.com",
+      };
+      } else if (code == "90FTESA") {
+      volunteeringps = {
+        "name": "Fusate",
+        "contacto": "info@fusate.org",
+      };
+      } else if (code == "20LDIESA") {
+      volunteeringps = {
+        "name": "Fusate",
+        "contacto": "kromero@ldielsalvador.org",
+      };
+     } else {
+      volunteeringps = {
+        "name": "Not found",
+      };
+      statusCode = 404;
+
+      response.status(200).send(volunteeringps)
+  };
+});
+     
+};
+  
+const volunteeringpsRouter = new volunteeringpsControllers();
+module.export = volunteeringpsRouter.router;

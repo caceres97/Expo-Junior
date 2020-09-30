95390ae0f43bf0cfcf534171dd53c56997167db2
Erika Martinez <erika.martinez2021@poma.superate.org.sv>
2020-09-30 14:21:40 -0600

    Creación de index

---
+++ index.js
@@ -0,0 +1,13 @@
+const   Controller_para_empresa_comidas = require("./Controllers/Controller_para_empresa_comidas");
+const Controller_para_empresa_salones = require("./Controllers/Controller_para_empresa_salones");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.use("/empresas", Controller_para_empresa_comidas);
+app.use("/empresas", Controller_para_empresa_salones);
+
+app.listen(3000, function () {
+  console.log("Cool Zone in port 3000!");
+});

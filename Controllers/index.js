c94727c1245856def56b045aa3348e72375ab870
Mariana Chávez <mariana.chavez2021@poma.superate.org.sv>
2020-10-12 17:11:31 -0600

    Correcciones en los CRUDS

---
+++ Controllers/index.js
@@ -0,0 +1,17 @@
+const   Controller_para_empresa_comidas = require("./Controllers.js/Controller_para_empresa_comidas");
+const Controller_para_empresa_salones = require("./Controllers.js/Controller_para_empresa_salones");
+const Controller_para_empresa_salones = require("./Controllers.js/Controller_para_empresa_veterinarias");
+const Controller_para_empresa_salones = require("./Controllers.js/Controller_para_empresa_clinicas");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.use("/empresas", Controller_para_empresa_comidas);
+app.use("/empresas", Controller_para_empresa_salones);
+app.use("/empresas", Controller_para_empresa_veterinarias);
+app.use("/empresas", Controller_para_empresa_clinicas);
+
+app.listen(3000, function () {
+  console.log("Cool Zone in port 3000!");
+});
